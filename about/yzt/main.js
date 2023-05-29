a34e72ca4d53c49e51283b8ff33d00832584df48
Ashenです <74820861+ASHENONEzz@users.noreply.github.com>
2023-05-29 22:43:12 +0800

    Team members informationa😎

---
+++ about/yzt/main.js
@@ -0,0 +1,129 @@
+function load2D_bg(){
+    var background = document.getElementById("background");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            
+                    
+            background.style.backgroundPositionX = xValue * 1 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
+        })
+    },false);
+
+}
+
+function load2D_bg2(){
+    var background = document.getElementById("background2");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            
+            background.style.backgroundPositionX = xValue *1 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
+        })
+    },false);
+
+}
+
+function load2D_bg2_2(){
+    var background = document.getElementById("background2");
+    document.onmousemove=function(){
+        console.log(window.outerHeight);
+        background.style.opacity=1-(getMousePos()/window.outerHeight*2.5);
+    }
+}
+
+function getMousePos(event) {
+    var e = event || window.event;
+    var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
+    var y = e.pageY || e.clientY + scrollY;
+    return y;
+}
+
+function load2D_lihui(){
+    var background = document.getElementById("lihui");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var xValue = calcValue(x, window.innerWidth);
+
+            background.style.left = xValue * 0.7-100 + "px";
+            
+        })
+    },false);
+
+}
+
+function load2D_yinghua(){
+    var background = document.getElementById("yinghua");
+    var range = 40;
+    var calcValue = function calcValue(a, b) {
+        return (a / b * range - range / 2).toFixed(1);
+    };
+    var timeout = void 0;
+    document.addEventListener('mousemove',
+    function(_ref) {
+        var x = _ref.x,
+        y = _ref.y;
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(function() {
+            var yValue = calcValue(y, window.innerHeight);
+            var xValue = calcValue(x, window.innerWidth);
+            background.style.backgroundPositionX = xValue * 1.5 -200+ "px ";
+            background.style.backgroundPositionY =  (-yValue * 1-50 ) + "px";
+        })
+    },false);
+
+}
+var i=0;
+
+function aboutMe(){
+    var slogan = document.getElementById("slogan");
+    var slogan2 = document.getElementById("slogan2");
+    if(i==0){
+        i=1;
+        slogan.style.height="100px";
+        slogan2.innerHTML="这里是杨宗韬，一个19岁青年，这个废柴能熟练掌握面向百度编程、Windows和Linux的开关机、PS和PR的安装和卸载、还喜欢打篮球、CSGO和制作MMD，另外附上鄙人的B站主页，上面都是我制作的MMD，有兴趣可以支持一下。希望可以通过这次的小组大作业结识更多的新朋友，同时也与新朋友一起学习共同进步。";
+    } else {
+        i=0;
+        slogan.style.height="50px";
+        slogan2.innerHTML="组长、CIO、素材、美工";
+    }
+    
+}
